f623b70dfcd16c90ec176c90f5d99cf1cc6d1a22
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-27 14:38:35 +0200

    implement user profiles

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import fields
+from .models import Profile
 
 
 class UserRegistrationForm(forms.ModelForm):
@@ -17,3 +19,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords dont't match.")
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
