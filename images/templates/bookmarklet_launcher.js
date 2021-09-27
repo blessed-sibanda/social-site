55ae5bc46c16299669e5b16390649713cc02b5cd
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-27 19:47:58 +0200

    generate image thumbnails

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -6,7 +6,7 @@
     let script = document.createElement('script');
     script.setAttribute(
       'src',
-      'https://127.0.0.1:8000/static/js/bookmarklet.js?r=' +
+      'https://mysite.com:8000/static/js/bookmarklet.js?r=' +
         Math.floor(Math.random() * Date.now()),
     );
 
