9665ba3f96845ed6f91e636220f00f368e3f37d8
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-27 21:13:36 +0200

    implement ajax infinte scrolling for image list

--- images/views.py
+++ images/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage,\
+    PageNotAnInteger
 from django.views.decorators.http import require_POST
 from .forms import ImageCreateForm
 from .models import Image
@@ -49,3 +51,25 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # if page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    ctx = {'section': 'images', 'images': images}
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html', ctx)
+    return render(request, 'images/image/list.html', ctx)
