0e29c5c3f7123283e616c93796d991e6ce323d53
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-27 10:31:23 +0200

    switch to postgresql database

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_site',
+        'USER': 'social_site',
+        'PASSWORD': '1234pass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
