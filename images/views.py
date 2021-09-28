65c65ece55f0e3f5caa6dd42a00de87ef53f7b39
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-28 13:39:07 +0200

    rank images according to # of views

--- images/views.py
+++ images/views.py
@@ -41,6 +41,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
     total_views = r.incr(f'image:{image.id}:views')
+    # increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image,
                    'total_views': total_views})
@@ -86,3 +88,20 @@ def image_list(request):
     if request.is_ajax():
         return render(request, 'images/image/list_ajax.html', ctx)
     return render(request, 'images/image/list.html', ctx)
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(
+        id__in=image_ranking_ids
+    ))
+    # sort the image objects by their index of appearance in 'image ranking'
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                  'images/image/ranking.html',
+                  {'section': 'images',
+                   'most_viewed': most_viewed})
