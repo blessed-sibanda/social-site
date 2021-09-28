751a7d99453c81d1a54ad715a35faf3e20ad0ed6
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-28 13:04:21 +0200

    add total-likes to images admin

--- images/admin.py
+++ images/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Image
 
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'image', 'created')
+    list_display = ('title', 'slug', 'image', 'created', 'total_likes')
     list_filter = ('created',)
